8ba4e20d8cc32cb041a3bb97b91537d087acf8aa
vanceleon <vanceleon44@gmail.com>
2018-09-13 22:58:02 -0400

    everything I committed before is being committed now

--- knexfile.js
+++ knexfile.js
@@ -6,39 +6,12 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
+    },
+    usNullAsDefault: true, 
+    migrations: {
+      directory: './db/migrations'
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
