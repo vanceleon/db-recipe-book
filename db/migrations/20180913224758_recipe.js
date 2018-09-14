b3a4386fd8362dfa535449cef0af3559ab9f7de7
vanceleon <vanceleon44@gmail.com>
2018-09-13 23:08:01 -0400

    latest

--- db/migrations/20180913224758_recipe.js
+++ db/migrations/20180913224758_recipe.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("recipes", (tbl) => {
+      tbl.increments();
+      tbl.string("name", 128).notNullable();
+      tbl
+        .integer("dish_id")
+        .unsigned()
+        .notNullable()
+        .reference("id")
+        .inTable("dishes");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("recipes");
 };
--- db/seeds/01-dishes.js
+++ db/seeds/01-dishes.js
@@ -4,10 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('dishes').insert([
+        { name: "burgers"},
+        { name: "pizza"},
+        { name: "tacos"}      ]);
     });
 };
